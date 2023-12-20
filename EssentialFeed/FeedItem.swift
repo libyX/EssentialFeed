12c709414def4b9f10b8b0ecf6602e1aa53d3e71
Libymol Xavier <libymolxavier@gmail.com>
2023-12-20 15:22:56 -0500

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Libymol Xavier on 12/20/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
